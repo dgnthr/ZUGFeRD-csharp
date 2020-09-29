748d09acbb1335f20953464a7d875cd903ae8a18
Dominik Günther <dg@tetratec.de>
2020-09-29 12:39:53 +0200

    Profile XRechnung does not like AppliedTradeAllowanceCharge -> BasisAmount

--- ZUGFeRD/InvoiceDescriptor21Writer.cs
+++ ZUGFeRD/InvoiceDescriptor21Writer.cs
@@ -271,7 +271,7 @@ namespace s2industries.ZUGFeRD
                         #endregion
 
                         #region BasisAmount
-                        Writer.WriteStartElement("ram:BasisAmount", profile: Profile.Extended | Profile.XRechnung1 | Profile.XRechnung);
+                        Writer.WriteStartElement("ram:BasisAmount", profile: Profile.Extended);
                         Writer.WriteValue(_formatDecimal(tradeAllowanceCharge.BasisAmount, 2));
                         Writer.WriteEndElement();
                         #endregion
